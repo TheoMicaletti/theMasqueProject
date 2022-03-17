db6a823deeac83115c259d221492dc6b7b2f7b91
MarcHazime <marc.hazime@gmail.com>
2022-03-17 17:25:52 +0100

    nouveau formulaire + aykut champion

--- src/form.js
+++ src/form.js
@@ -3,3 +3,9 @@ const message = "Merci pour votre idée !";
 document.getElementById("myForm").addEventListener("submit", function () {
   alert(message);
 });
+
+const butt = document.getElementById('myForm');
+
+butt.addEventListener('submit', function(event) {
+    event.target.disabled = true;
+});
